03ccac96470a6995fdb7ad06d9b161f2633f7115
Poonsak Nuchsiri <poonsak.nuch@gmail.com>
2018-06-25 15:29:15 +0700

    Updating /subscribe API

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 
 let db;
 const app = express();
-// const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(express.static('public'));
 
@@ -16,52 +13,48 @@ app.get('/', function (req, res) {
 app.get('/subscribe', function (req, res) {
     let token = generateToken();
     let subscribe = {};
-    var noo = false;
-    // subscribe = JSON.parse(fs.readFileSync('./names.json', 'utf8'));
-
     openDB();
     subscribe['name'] = req.query.name;
     subscribe['reclamationToken'] = req.query.reclamationToken;
+
     let json = JSON.parse(JSON.stringify(subscribe));
+
     subscribe['desc'] = req.query.desc;
     subscribe['email'] = req.query.email;
 
     let sql = 'SELECT name name, reclamation_token token FROM domain ' +
         'WHERE name = ? and reclamation_token  = ?';
     db.serialize(() => {
-        let domainAvailable = false;
-        db.get(sql, [subscribe['name'],subscribe['reclamationToken']], (err, row) => {
+        db.get(sql, [subscribe['name'], subscribe['reclamationToken']], (err, row) => {
             if (err) {
                 return console.error(err.message);
             }
-            // return row
-            //     ? (console.log(row.name))
-            //     : console.log("This domain is available.")
             if (row) {
-                console.log(row.name)
+                console.log(row.name);
             } else {
-                console.log("This domain is available.")
+                console.log("This domain is available.");
             }
         });
-        let domain = [
-            subscribe['name'],
-            subscribe['desc'],
-            subscribe['email'],
-            subscribe['reclamationToken']
-        ];
-        let placeholders = '(?,?,?,?)';
-        sql = 'INSERT INTO domain VALUES ' + placeholders;
-        db.run(sql, domain, function (err) {
-            if (err) {
-                return console.error(err.message);
-            }
-            console.log('Rows inserted')
-        });
+
+            let domain = [
+                subscribe['name'],
+                subscribe['desc'],
+                subscribe['email'],
+                subscribe['reclamationToken']
+            ];
+            let placeholders = '(?,?,?,?)';
+            sql = 'INSERT INTO domain VALUES ' + placeholders;
+            db.run(sql, domain, function (err) {
+                if (err) {
+                    res.status(400);
+                    // return console.error(err.message);
+                    return console.error("Domain not available.")
+                }
+                console.log('Rows inserted')
+            });
         closeDB();
     });
-    if (noo) {
-        console.log("noasdnasod")
-    }
+    console.log("_____________________");
     res.send(json);
 });
 
@@ -70,7 +63,7 @@ function openDB() {
         if (err) {
             console.error(err.message);
         }
-        console.log('Connected to the domain database.');
+        // console.log('Connected to the domain database.');
     });
 }
 
@@ -80,15 +73,7 @@ function closeDB() {
             console.error(err.message);
         }
     });
-    console.log('Close the database connection.');
-}
-
-function generateToken() {
-    let token = "";
-    for (let i = 0; i < 6; i++) {
-        token = token + Math.random().toString(36).substr(2, 6);
-    }
-    return token
+    // console.log('Close the database connection.');
 }
 
 const server = app.listen(8081, function () {
